8f99065891884b315ced6eb178cfedd6376a6453
Kover CHENG <jueming.cheng@gmail.com>
2019-05-28 14:47:21 +0800

    Problem905

--- src/Problem905_SortArrayByParity.java
+++ src/Problem905_SortArrayByParity.java
@@ -1,13 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Problem905_SortArrayByParity
 {
-	public int[] sortArrayByParity(int[] A)
+	private static int[] sortArrayByParity(int[] A)
 	{
+		int i = 0;
+		int j = A.length - 1;
 		
-		return null;
+		while (i < j)
+		{
+			while (i < A.length && 0 == A[i] % 2) i++;
+			while (j >= 0 && 0 != A[j] % 2) j--;
+			if (i < j)
+			{
+				A[i] += A[j];
+				A[j] = A[i] - A[j];
+				A[i] -= A[j];
+			}
+		}
+		return A;
 	}
 	
 	public static void main(String[] arg)
 	{
-	
+		Scanner inputScanner = new Scanner(System.in);
+		String inputString = inputScanner.nextLine();
+		inputScanner.close();
+		
+		String[] inputStringNumbers = inputString.substring(1, inputString.length() - 1).split(",");
+		int[] inputNumbers = Arrays.stream(inputStringNumbers).mapToInt(Integer::parseInt).toArray();
+		
+		System.out.println(Arrays.toString(sortArrayByParity(inputNumbers)));
 	}
 }
---
+++ src/Problem905_SortArrayByParity_BetterSolution.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Problem905_SortArrayByParity_BetterSolution
+{
+	
+	public int[] sortArrayByParity_1(int[] A)
+	{
+		Integer[] B = new Integer[A.length];
+		for (int t = 0; t < A.length; ++ t)
+			B[t] = A[t];
+		
+		Arrays.sort(B, Comparator.comparingInt(a -> a % 2));
+		
+		
+		for (int t = 0; t < A.length; ++ t)
+			A[t] = B[t];
+		return A;
+	}
+	
+	public int[] sortArrayByParity_2(int[] A)
+	{
+		return Arrays.stream(A)
+				.boxed()
+				.sorted(Comparator.comparingInt(a -> a % 2))        //.sorted((a, b) -> Integer.compare(a % 2, b % 2))
+				.mapToInt(i -> i)
+				.toArray();
+	}
+	
+	public int[] sortArrayByParity_3(int[] A)
+	{
+		int n = A.length;
+		int []b = new int[n];
+		int j = 0, k = n - 1;
+		
+		for (int a : A)
+			if (a % 2 == 0)
+				b[j++] = a;
+			else
+				b[k--] = a;
+		return b;
+	}
+}
